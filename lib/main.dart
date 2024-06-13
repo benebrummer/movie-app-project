758aaf0f11bfbcee4442c354b24c8ac76a6e35bb
benebrummer <benedikt.brummer@gmx.net>
2024-06-13 19:58:46 +0200

    init empty project.

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MovieApp());
+}
+
+class MovieApp extends StatelessWidget {
+  const MovieApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Flutter Demo Home Page'),
+        ),
+      ),
+    );
+  }
+}
