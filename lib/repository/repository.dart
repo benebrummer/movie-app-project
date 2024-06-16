8ddc2475dfa62175854fa4a2851af6f613266721
benebrummer <benedikt.brummer@gmx.net>
2024-06-16 11:29:20 +0200

    Implemented some movie calls.

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,10 +1,9 @@
-import '../entity/media_type.dart';
 import '../entity/movie.dart';
 
 abstract class TmdbRepository {
   Future<List<Movie>> searchMovie(final String query);
-  Future<List<Movie>> getPopularMovies(final MediaType mediaType);
-  Future<List<Movie>> getTopRatedMovies(final MediaType mediaType);
+  Future<List<Movie>> getPopularMovies();
+  Future<List<Movie>> getTopRatedMovies();
   Future<List<Movie>> getUpcomingMovies();
   Future<List<Movie>> getNowPlayingMovies();
 }
--- lib/repository/tmdb_repository.dart
+++ lib/repository/tmdb_repository.dart
@@ -3,51 +3,70 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movie_app_project/repository/repository.dart';
 
-import '../entity/media_type.dart';
 import '../entity/movie.dart';
 import '../env/env.dart';
 
 class MediaRepository extends TmdbRepository {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
   final String apiKey = Env.tmdbApiKey;
-  static const String baseUrl = 'https://api.themoviedb.org/3/';
 
   @override
   Future<List<Movie>> searchMovie(final String query) async {
     // TODO: add paging
     final response = await http
-        .get(Uri.parse('${baseUrl}search/movie?api_key=$apiKey&query=$query'));
-
+        .get(Uri.parse('$baseUrl/search/movie?api_key=$apiKey&query=$query'));
     if (response.statusCode != 200) {
       throw Exception('Failed to search movie');
     }
-
     // paging?
-    final Map<String, dynamic> decodedJson = jsonDecode(response.body);
-    final List<dynamic> results = decodedJson['results'];
-    return results.map((json) => Movie.fromJson(json)).toList();
+    return _mapResponseToMovieList(response.body);
   }
 
   @override
-  Future<List<Movie>> getPopularMovies(final MediaType mediaType) async {
-    // TODO: implement getPopularMovies
-    throw UnimplementedError();
+  Future<List<Movie>> getPopularMovies() async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/movie/popular?api_key=$apiKey'));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to get popular movies');
+    }
+    return _mapResponseToMovieList(response.body);
   }
 
   @override
-  Future<List<Movie>> getTopRatedMovies(final MediaType mediaType) async {
-    // TODO: implement getTopRatedMovies
-    throw UnimplementedError();
+  Future<List<Movie>> getTopRatedMovies() async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/movie/top_rated?api_key=$apiKey'));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to get top rated movies');
+    }
+    return _mapResponseToMovieList(response.body);
   }
 
   @override
   Future<List<Movie>> getUpcomingMovies() async {
-    // TODO: implement getUpcomingMovies
-    throw UnimplementedError();
+    // TODO: dates in response?
+    final response =
+        await http.get(Uri.parse('$baseUrl/movie/upcoming?api_key=$apiKey'));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to get upcoming movies');
+    }
+    return _mapResponseToMovieList(response.body);
   }
 
   @override
   Future<List<Movie>> getNowPlayingMovies() async {
-    // TODO: implement getNowPlayingMovies
-    throw UnimplementedError();
+    // TODO: dates in response?
+    final response =
+        await http.get(Uri.parse('$baseUrl/movie/now_playing?api_key=$apiKey'));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to get now playing movies');
+    }
+    return _mapResponseToMovieList(response.body);
+  }
+
+  List<Movie> _mapResponseToMovieList(final String response) {
+    final Map<String, dynamic> decodedJson = jsonDecode(response);
+    final List<dynamic> results = decodedJson['results'];
+    return results.map((json) => Movie.fromJson(json)).toList();
   }
 }
