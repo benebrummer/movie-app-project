33e60a5521996d7cf0a11b738de02f415b73a367
benebrummer <benedikt.brummer@gmx.net>
2024-06-23 15:51:55 +0200

    Added details methods.

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,20 +1,26 @@
-import '../entity/movie.dart';
-import '../entity/review.dart';
-import '../entity/tv_series.dart';
+import 'package:movie_app_project/entity/media/movie/movie_details.dart';
+import 'package:movie_app_project/entity/media/series/series_details.dart';
+
+import '../entity/media/movie/movie.dart';
+import '../entity/media/review/review.dart';
+import '../entity/media/series/tv_series.dart';
 
 abstract class TmdbRepository {
   // TODO: check discover endpoint
-  Future<List<Movie>> searchMovie(final String query);
+  Future<Map<dynamic, dynamic>> searchMovie(final String query, final int page);
   Future<List<Movie>> getPopularMovies();
   Future<List<Movie>> getTopRatedMovies();
   Future<List<Movie>> getUpcomingMovies();
   Future<List<Movie>> getNowPlayingMovies();
   Future<List<Review>> getMovieReviews(final int movieId);
+  Future<MovieDetails> getMovieDetails(final int movieId);
 
-  Future<List<TvSeries>> searchSeries(final String query);
+  Future<Map<dynamic, dynamic>> searchSeries(
+      final String query, final int page);
   Future<List<TvSeries>> getPopularSeries();
   Future<List<TvSeries>> getTopRatedSeries();
   Future<List<TvSeries>> getAiringTodaySeries();
   Future<List<TvSeries>> getOnTheAirSeries();
   Future<List<Review>> getSeriesReviews(final int seriesId);
+  Future<SeriesDetails> getSeriesDetails(final int seriesId);
 }
