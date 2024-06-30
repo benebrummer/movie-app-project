85d127b60467ef8699bfe4482670e0fd7b5ec9e3
benebrummer <benedikt.brummer@gmx.net>
2024-06-30 12:26:50 +0200

    Separated review item.

--- lib/pages/reviews/reviews_page.dart
+++ lib/pages/reviews/reviews_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
+import 'package:movie_app_project/pages/reviews/widgets/review_list_item.dart';
 
 import '../../controller/review_controller.dart';
 
@@ -37,82 +37,12 @@ class ReviewsPage extends StatelessWidget {
                             ),
                             borderRadius: BorderRadius.circular(10),
                           ),
-                          child: ListTile(
-                            contentPadding: const EdgeInsets.all(20),
-                            title: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    ClipOval(
-                                      child: review
-                                              .authorDetails.avatarPath.isEmpty
-                                          ? const Icon(Icons.person, size: 50)
-                                          : Image.network(
-                                              'https://image.tmdb.org/t/p/w200/${review.authorDetails.avatarPath}',
-                                              width: 50,
-                                              height: 50,
-                                            ),
-                                    ),
-                                    const SizedBox(width: 10),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          review.author,
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        Text(
-                                          'Rating: ${review.authorDetails.rating == null ? '-' : review.authorDetails.rating.toString()}',
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 10),
-                                const Divider(),
-                              ],
-                            ),
-                            subtitle: Padding(
-                              padding: const EdgeInsets.only(top: 20.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(review.content),
-                                  const SizedBox(height: 10),
-                                  const Divider(),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    'Created at: ${_formatDateTime(review.createdAt)}',
-                                    style: const TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 12,
-                                    ),
-                                  ),
-                                  Text(
-                                    'Updated at: ${_formatDateTime(review.updatedAt)}',
-                                    style: const TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 12,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+                          child: ReviewListTile(review: review),
                         ),
                       );
                     },
                   ),
       ),
     );
-  }
-
-  String _formatDateTime(final DateTime dateTime) {
-    DateFormat formatter = DateFormat('yyyy/MM/dd HH:mm');
-    return formatter.format(dateTime);
   }
 }
---
+++ lib/pages/reviews/widgets/review_list_item.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../entity/media/review/review.dart';
+
+class ReviewListTile extends StatelessWidget {
+  final Review review;
+  const ReviewListTile({super.key, required this.review});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.all(20),
+      title: Column(
+        children: [
+          Row(
+            children: [
+              ClipOval(
+                child: review.authorDetails.avatarPath.isEmpty
+                    ? const Icon(Icons.person, size: 50)
+                    : Image.network(
+                        'https://image.tmdb.org/t/p/w200/${review.authorDetails.avatarPath}',
+                        width: 50,
+                        height: 50,
+                      ),
+              ),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    review.author,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Rating: ${review.authorDetails.rating == null ? '-' : review.authorDetails.rating.toString()}',
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          const Divider(),
+        ],
+      ),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(review.content),
+            const SizedBox(height: 10),
+            const Divider(),
+            const SizedBox(height: 10),
+            Text(
+              'Created at: ${_formatDateTime(review.createdAt)}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 12,
+              ),
+            ),
+            Text(
+              'Updated at: ${_formatDateTime(review.updatedAt)}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _formatDateTime(final DateTime dateTime) {
+    DateFormat formatter = DateFormat('yyyy/MM/dd HH:mm');
+    return formatter.format(dateTime);
+  }
+}
