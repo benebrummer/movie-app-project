445dcfdb0e89a484255b1be6c6b831992158ba60
benebrummer <benedikt.brummer@gmx.net>
2024-06-30 12:27:51 +0200

    Adapted padding from search page.

--- lib/pages/favorites/favorites_page.dart
+++ lib/pages/favorites/favorites_page.dart
@@ -17,40 +17,43 @@ class FavoritesPage extends StatelessWidget {
         title: const Text('Favorites'),
       ),
       drawer: MovieAppDrawer(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Obx(
-              () {
-                return ToggleButtons(
-                  constraints: const BoxConstraints(
-                    minHeight: 50.0,
-                    minWidth: 100.0,
-                  ),
-                  textStyle: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
-                  onPressed: (int index) {
-                    _favoritesController.updateMediaType(index);
-                  },
-                  isSelected: _favoritesController.selectedMediaTypes,
-                  children: List.of(
-                      {const Text('Movies'), const Text('TV Shows')},
-                      growable: false),
-                );
-              },
+      body: Padding(
+        padding: const EdgeInsets.only(top: 13.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Obx(
+                () {
+                  return ToggleButtons(
+                    constraints: const BoxConstraints(
+                      minHeight: 50.0,
+                      minWidth: 100.0,
+                    ),
+                    textStyle: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                    onPressed: (int index) {
+                      _favoritesController.updateMediaType(index);
+                    },
+                    isSelected: _favoritesController.selectedMediaTypes,
+                    children: List.of(
+                        {const Text('Movies'), const Text('TV Shows')},
+                        growable: false),
+                  );
+                },
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Obx(() => _favoritesController.selectedMediaTypes[0]
-                  ? FavoriteMovieGrid()
-                  : FavoriteSeriesGrid()),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Obx(() => _favoritesController.selectedMediaTypes[0]
+                    ? FavoriteMovieGrid()
+                    : FavoriteSeriesGrid()),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
